e8378d4fe559dbaf6f9ca77988458cd71d905d27
kameoman <spiderman.935963@gmail.com>
2021-08-30 18:30:21 +0900

    fix

--- main.py
+++ main.py
@@ -34,25 +34,25 @@ import streamlit as st
 
 st.title('文字起こしアプリ')
 st.header('概要')
-st.write('こちらはGoogle Cloud Speech-to-Textを利用した文字起こしアプリです。リンクは下記です。')
-st.markdown('<a href="https://cloud.google.com/speech-to-text?hl=ja">Cloud Speech-to-Text</a>', unsafe_allow_html=True)
+# st.write('こちらはGoogle Cloud Speech-to-Textを利用した文字起こしアプリです。リンクは下記です。')
+# st.markdown('<a href="https://cloud.google.com/speech-to-text?hl=ja">Cloud Speech-to-Text</a>', unsafe_allow_html=True)
 
-upload_file = st.file_uploader('ファイルのアップロード', type=['mp3', 'wav'])
-if upload_file is not None:
-    content = upload_file.read()
-    st.subheader('ファイル詳細')
-    file_details = {'FileName': upload_file.name, 'FileType': upload_file.type, 'FileSize': upload_file.size}
-    st.write(file_details)
-    st.subheader('音声の再生')
-    st.audio(content)
-    st.subheader('言語選択')
-    option = st.selectbox('翻訳言語を選択してください',
-                         ('英語', '日本語', 'スペイン語'))
-    st.write('選択中の言語：', option)
+# upload_file = st.file_uploader('ファイルのアップロード', type=['mp3', 'wav'])
+# if upload_file is not None:
+#     content = upload_file.read()
+#     st.subheader('ファイル詳細')
+#     file_details = {'FileName': upload_file.name, 'FileType': upload_file.type, 'FileSize': upload_file.size}
+#     st.write(file_details)
+#     st.subheader('音声の再生')
+#     st.audio(content)
+#     st.subheader('言語選択')
+#     option = st.selectbox('翻訳言語を選択してください',
+#                          ('英語', '日本語', 'スペイン語'))
+#     st.write('選択中の言語：', option)
     
-    st.write('文字起こし')
-    if st.button('開始'):
-        comment = st.empty()
-        comment.write('文字起こしを開始します')
-        transcribe_file(content, lang=option)
-        comment.write('完了しました')
+#     st.write('文字起こし')
+#     if st.button('開始'):
+#         comment = st.empty()
+#         comment.write('文字起こしを開始します')
+#         transcribe_file(content, lang=option)
+#         comment.write('完了しました')
